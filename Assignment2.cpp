4c18ecfd2018b5988c72f9d359232ca3d921ddd7
fkaruvally <98418727+fkaruvally@users.noreply.github.com>
2022-02-04 13:35:14 -0600

    Submit Assignment 2

---
+++ Assignment2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int miles;
+    miles = 4276;
+
+    cout << "We have made a long journey of " << miles << "miles. " << endl;
+
+    int balance; 
+    balance = -20;
+
+    cout << "Our checking account balance is " << balance << ". " << endl;
+
+    int days; 
+    days = 18900;
+
+    cout << "About " << days << "days ago Columbus stood on this spot." << endl;
+
+    return 0;
+}
