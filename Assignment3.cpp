c6f5fa69e14144edef8e7c7d74f198f060978ace
fkaruvally <98418727+fkaruvally@users.noreply.github.com>
2022-02-04 13:53:55 -0600

    Submit Assignment 3

---
+++ Assignment3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char letterA; 
+    letterA = 65;
+
+    cout << letterA << endl;
+
+    char letterB; 
+    letterB = 66;
+
+    cout << letterB << endl;
+
+    return 0;
+}
